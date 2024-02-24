736305bcb0b2b3a2b5b6779ece23943d7ef225b5
abhay gupta <akg568794@gmail.com>
2024-02-24 11:31:43 +0530

    Created get user listing api route

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
+
 const Home = () => {
   return (
     <div>
-      Home
+      {/* <img width="1600" height="200" src="https://www.squareyards.com/cdn-cgi/image/width=1366,height=560,quality=80,fit=crop,gravity=auto,format=webp/assets/images/desktop-banner.jpeg" alt="home banner" /> */}
+
     </div>
   )
 }
